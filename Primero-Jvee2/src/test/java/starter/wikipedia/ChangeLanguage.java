176984bf1cdedc51b5b68b811c3eff6d15306c3b
jamjamprimero <jayveeprimero@gmail.com>
2023-01-11 00:58:36 +0800

    Changed Codes

--- Primero-Jvee2/src/test/java/starter/wikipedia/ChangeLanguage.java
+++ Primero-Jvee2/src/test/java/starter/wikipedia/ChangeLanguage.java
@@ -2,7 +2,6 @@ package starter.wikipedia;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +10,7 @@ import net.serenitybdd.core.Serenity;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.thucydides.core.annotations.Managed;
 import pageobjects.wikipedia.NavigateWiki;
+import pageobjects.wikipedia.ValidationActions;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 public class ChangeLanguage {
@@ -23,22 +23,22 @@ public class ChangeLanguage {
 	@Test
 	public void change_language_to_spanish() {
 		nav.go_to_wikipedia();
-		nav.click_page_information();	
-		Serenity.reportThat("Validate Page Content Language is \"en - English\"",
-				() -> assertThat(vac.isLanguageInfoEqualTo("en - English")));	
-		nav.navigate_back(driver);	
-		Serenity.reportThat("Validate user is redirected back to Wiki Main page",
-				() -> assertThat(driver.getCurrentUrl()).isEqualTo("https://en.wikipedia.org/wiki/Main_Page"));	
+		
+		Serenity.reportThat("Validate Page Content Language is \"en\"",
+				() -> assertThat(vac.isLanguageInfoEqualTo("en")));	
+		
+//		nav.click_page_information();	
+//		Serenity.reportThat("Validate Page Content Language is \"en - English\"",
+//				() -> assertThat(vac.isLanguageInfoEqualTo("en - English")));	
+//		nav.navigate_back(driver);	
+//		Serenity.reportThat("Validate user is redirected back to Wiki Main page",
+//				() -> assertThat(driver.u).isEqualTo("https://en.wikipedia.org/wiki/Main_Page"));	
 		nav.select_es_language();		
-		Serenity.reportThat("Validate user is redirected to Spanish Wiki page",
-				() -> assertThat(driver.getCurrentUrl()).isEqualTo("https://es.wikipedia.org/wiki/Wikipedia:Portada"));
-		nav.click_page_information();
-		Serenity.reportThat("Validate Page Content Language is \"es - español\"",
-				() -> assertThat(vac.isLanguageInfoEqualTo("es - español")));
-	}
-	
-	@Test
-	public void tearDown() {
+//		Serenity.reportThat("Validate user is redirected to Spanish Wiki page",
+//				() -> assertThat(driver.getCurrentUrl()).isEqualTo("https://es.wikipedia.org/wiki/Wikipedia:Portada"));
+//		nav.click_page_information();
+		Serenity.reportThat("Validate Page Content Language is \"es\"",
+				() -> assertThat(vac.isLanguageInfoEqualTo("es")));
 		driver.quit();
 	}
 }
